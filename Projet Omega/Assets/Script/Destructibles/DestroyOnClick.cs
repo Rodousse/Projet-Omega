6fc812a8163a8f0a0a2a5adbe0a393bd1c29fa87
YoueIUT <Youe.Graillot@etu.udamail.fr>
2015-10-16 18:58:35 +0200

    Ajout de caisses destructibles
    + prefab caisse
    + script DestroyOnClick

---
+++ Projet Omega/Assets/Script/Destructibles/DestroyOnClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnClick : MonoBehaviour
+{
+	public bool nextToPlayer = false;
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.transform.parent.CompareTag("Player"))
+			nextToPlayer = true;
+    }
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.transform.parent.CompareTag("Player"))
+			nextToPlayer = false;
+	}
+
+	void OnMouseDown()
+	{
+		if (nextToPlayer)
+			Destroy(gameObject);
+	}
+}
