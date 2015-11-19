7af6fc7d3c4a3129d50c8adf74f140e972043f05
YoueIUT <Youe.Graillot@etu.udamail.fr>
2015-11-19 02:07:42 +0100

    MAJ
    editeur corrigé : overlay
    ajouts de nouveaux blocs
    modif de la lum

---
+++ Projet Omega/Assets/Script/Editeur/ChoixEvents.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChoixEvents : MonoBehaviour {
+	
+	RawImage Image;
+	Controleur Editeur;
+	UI_Palette Palette;
+
+	void Start () {
+		Image = GetComponent<RawImage>();
+		Editeur = GetComponentInParent<Controleur>();
+		Palette = GetComponentInParent<UI_Palette>();
+
+		EventTrigger thisEventTrigger = GetComponent<EventTrigger>();
+	}
+	
+	public void Begin()
+	{
+		Image.enabled = false;
+		Palette.Shift();
+        Editeur.setTile(gameObject.name);
+    }
+
+	public void Move()
+	{
+		Editeur.MovePreview();
+	}
+
+	public void End()
+	{
+		Image.enabled = true;
+		Editeur.Placer();
+	}
+}
--- Projet Omega/Assets/Script/Editeur/Controleur.cs
+++ Projet Omega/Assets/Script/Editeur/Controleur.cs
@@ -1,5 +1,11 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
+public class SpriteCol : System.Object
+{
+	public Sprite Box, Cage, Dirt, Fire, Grass, IronBox, IronWall_Bright, IronWall_Dark, Light;
+}
+
 public class Controleur : MonoBehaviour
 {
 	public tk2dTileMap TileMap;
@@ -8,43 +14,55 @@ public class Controleur : MonoBehaviour
 
 	public Transform PreviewTile;
 
+	public SpriteCol Previews;
+
+	SpriteRenderer PreviewBlocSprite;
+
 	Vector3 MousePosition, BlocPosition;
 
-	int TileID = 0, LayerID = 0, ToolID;
+	int TileID = 0, LayerID = 0, ToolID = 1;
 
 	void Start()
 	{
 		MousePosition.z = 0;
+		PreviewBlocSprite = PreviewBloc.GetComponent<SpriteRenderer>();
 	}
 
-	void Update()
+	public void MovePreview()
 	{
-		if (Input.GetButton("Fire1"))
-		{
-			MousePosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x + 1.25f;
-            MousePosition.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+		MousePosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x + 1.25f;
+		MousePosition.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
 
-			PreviewBloc.transform.position = new Vector3(MousePosition.x - 1.25f, MousePosition.y, 0);
+		PreviewBloc.transform.position = new Vector3(MousePosition.x - 1.25f, MousePosition.y, 0);
 
-			MousePosition.x = MousePosition.x - MousePosition.x % 2.5f;
-            MousePosition.y = MousePosition.y - MousePosition.y % 2.5f;
+		MousePosition.x = MousePosition.x - MousePosition.x % 2.5f;
+		MousePosition.y = MousePosition.y - MousePosition.y % 2.5f;
 
-			PreviewTile.transform.position = new Vector3(MousePosition.x, MousePosition.y + 1.25f, 0);
-			
-		}
-		else if(Input.GetButtonUp("Fire1"))
-		{
-			PreviewTile.transform.position = new Vector3(-2, 0, 0);
-			PreviewBloc.transform.position = new Vector3(-2,0, 0);
-			Placer();
-		}
+		PreviewTile.transform.position = new Vector3(MousePosition.x, MousePosition.y + 1.25f, 0);
 	}
 
 	public void Placer()
 	{
-		TileMap.BeginEditMode();
-		TileMap.SetTile((int) (MousePosition.x / 2.5f), (int) (MousePosition.y / 2.5f), LayerID, TileID);
-		TileMap.EndEditMode();
+		PreviewTile.transform.position = new Vector3(-2, 0, 0);
+		PreviewBloc.transform.position = new Vector3(-2, 0, 0);
+
+		switch (ToolID)
+		{
+			case 0: // Eraser
+				TileMap.BeginEditMode();
+				TileMap.SetTile((int)(MousePosition.x / 2.5f), (int)(MousePosition.y / 2.5f), LayerID, -1);
+				TileMap.EndEditMode();
+				break;
+			case 1:
+				TileMap.BeginEditMode();
+				TileMap.SetTile((int)(MousePosition.x / 2.5f), (int)(MousePosition.y / 2.5f), LayerID, TileID);
+				TileMap.EndEditMode();
+				break;
+			case 2:
+				break;
+			default:
+				break;
+		}
 	}
 
 	public void setTool(string INPUT)
@@ -67,49 +85,55 @@ public class Controleur : MonoBehaviour
 
 	public void setTile(string INPUT)
 	{
-		print("set tile");
-
 		switch (INPUT)
 		{
 			case "Box":
 				TileID = 7;
+				PreviewBlocSprite.sprite = Previews.Box;
 				LayerID = 2;
-                break;
+				break;
 			case "Dirt":
 				TileID = 6;
+				PreviewBlocSprite.sprite = Previews.Dirt;
 				LayerID = 0;
 				break;
 			case "Grass":
 				TileID = 0;
+				PreviewBlocSprite.sprite = Previews.Grass;
 				LayerID = 0;
 				break;
 			case "IronBox":
 				TileID = 3;
+				PreviewBlocSprite.sprite = Previews.IronBox;
 				LayerID = 0;
 				break;
-			case "IronWall1":
+			case "IronWall_Bright":
 				TileID = 4;
+				PreviewBlocSprite.sprite = Previews.IronWall_Bright;
 				LayerID = 1;
 				break;
-			case "IronWall2":
+			case "IronWall_Dark":
 				TileID = 1;
+				PreviewBlocSprite.sprite = Previews.IronWall_Dark;
+				LayerID = 1;
+				break;
+			case "Cage":
+				TileID = 2;
+				PreviewBlocSprite.sprite = Previews.Cage;
 				LayerID = 1;
 				break;
 			case "Fire":
 				TileID = 5;
+				PreviewBlocSprite.sprite = Previews.Fire;
 				LayerID = 2;
 				break;
 			case "Light":
 				TileID = 9;
+				PreviewBlocSprite.sprite = Previews.Light;
 				LayerID = 3;
 				break;
 			default:
 				break;
 		}
 	}
-
-	public void ChangePreviewSprite(Sprite INPUT)
-	{
-		PreviewBloc.GetComponent<SpriteRenderer>().sprite = INPUT;
-    }
 }
