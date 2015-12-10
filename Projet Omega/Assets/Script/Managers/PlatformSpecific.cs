316a8060a4414838601ed724ad15524ff0b3408c
YoueIUT <Youe.Graillot@etu.udamail.fr>
2015-12-10 21:20:18 +0100

    Update sans bug

---
+++ Projet Omega/Assets/Script/Managers/PlatformSpecific.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSpecific : MonoBehaviour {
+	
+	void Start () {
+		//Empeche l'écran de s'éteindre
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+}
